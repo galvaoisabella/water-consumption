9cf2e9a47fa7d35a349dc6c0deb1cfdc6eb96657
Isabella Galvão <isabella_asg@hotmail.com>
2021-10-23 17:39:37 -0300

    feat - rotina de envio de volume ao servidor

--- water-consumption.ino
+++ water-consumption.ino
@@ -10,27 +10,31 @@
 #endif
 
 #define BUTTON D3 // Botão flash arduino
+#define SENSOR D2 // Sensor
+#define DEBUG true
+#define LEDTEST false
 
 /*****************************************************************************************/
 /************************************** SENSOR *******************************************/
 /*****************************************************************************************/
-//definicao do pino do sensor e de interrupcao
-const int INTERRUPCAO_SENSOR = 0; //interrupt = 0 equivale ao pino digital 2
-const int PINO_SENSOR = 2;
+long currentMillis = 0;
+long previousMillis = 0;
+long previousMillisService = 0;
+int interval = 1000;
+long oneHour = 3600000;
+float calibrationFactor = 5;
+volatile byte pulseCount;
+byte pulse1Sec = 0;
+float flowRate;
+unsigned long flowMilliLitres;
+unsigned int totalMilliLitres;
+float flowLitres;
+float totalLitres;
 
-//definicao da variavel de contagem de voltas
-unsigned long contador = 0;
-
-//definicao do fator de calibracao para conversao do valor lido
-const float FATOR_CALIBRACAO = 4.5;
-
-//definicao das variaveis de fluxo e volume
-float fluxo = 0;
-float volume = 0;
-float volume_total = 0;
-
-//definicao da variavel de intervalo de tempo
-unsigned long tempo_antes = 0;
+void IRAM_ATTR pulseCounter()
+{
+  pulseCount++;
+}
 /*****************************************************************************************/
 
 /*****************************************************************************************/
@@ -51,12 +55,11 @@ WebsocketsClient client;
 // Led
 const int led = LED_BUILTIN;
 //const char* contador = 0;
-int estadobotao = LOW;
+int buttonState = LOW;
 
 
-void setup()
-{
-  // Iniciamos a serial com velocidade de 115200
+void setup() {
+  // Inicia a serial com velocidade de 115200
   Serial.begin(115200);
 
   // Definimos o pino como saída
@@ -65,7 +68,18 @@ void setup()
   pinMode(BUTTON, INPUT_PULLUP);
 
   //configuracao do pino do sensor como entrada em nivel logico alto
-  pinMode(PINO_SENSOR, INPUT_PULLUP);
+  pinMode(SENSOR, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
+
+  // Valor inicial variáveis
+  pulseCount = 0;
+  flowRate = 0.0;
+  flowMilliLitres = 0;
+  totalMilliLitres = 0;
+  previousMillis = 0;
+  previousMillisService = 0;
+
+
 
   WiFi.mode(WIFI_STA);
 
@@ -73,8 +87,7 @@ void setup()
   WiFi.begin(ssid, password);
 
   // Enquanto não conectar printamos um "."
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(1000);
   }
@@ -86,36 +99,36 @@ void setup()
   bool connected = client.connect(websockets_server_host, websockets_server_port, "/");
 
   // Se foi possível conectar
-  if (connected)
-  {
+  if (connected) {
     // Exibimos mensagem de sucesso
     Serial.println("Connected!");
     // Enviamos uma msg "Hello Server" para o servidor
     client.send("Hello Server");
   }   // Se não foi possível conectar
-  else
-  {
+  else {
     // Exibimos mensagem de falha
     Serial.println("Not Connected!");
     return;
   }
 
-  // Iniciamos o callback onde as mesagens serão recebidas
-  client.onMessage([&](WebsocketsMessage message)
+  // Callback onde as mesagens serão recebidas
+  client.onMessage([&](WebsocketsMessage message) 
   {
-    // Exibimos a mensagem recebida na serial
-    Serial.print("Got Message: ");
-    Serial.println(message.data());
+    if (LEDTEST) {
+      //Exibe mensagem recebida na serial
+      Serial.print("Got Message: ");
+      Serial.println(message.data());
 
-    // Ligamos/Desligamos o led de acordo com o comando
-    if (message.data().equalsIgnoreCase("ON"))
-      digitalWrite(led, HIGH);
-    else if (message.data().equalsIgnoreCase("OFF"))
-      digitalWrite(led, LOW);
+      // Teste: Liga/Desliga o led de acordo com o comando
+      if (message.data().equalsIgnoreCase("ON"))
+        digitalWrite(led, HIGH);
+      else if (message.data().equalsIgnoreCase("OFF"))
+        digitalWrite(led, LOW);
+      }
   });
+ 
 
-
-  // ArduinoOTA config
+  // Configuração ArduinoOTA
   ArduinoOTA.onStart([]() {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
@@ -153,72 +166,61 @@ void setup()
   Serial.println(WiFi.localIP());
 }
 
-void loop()
-{
+void loop() {
+  listenService();
+  readSensorFlow();
+}
+
+/**
+   Método que escuta informações enviadas do servidor
+*/
+void listenService() {
   ArduinoOTA.handle();
   //  De tempo em tempo, o websockets client checa por novas mensagens recebidas
   if (client.available())
     client.poll();
-
-  // Verifica se o botao reset foi pressionado
-  // estadobotao = digitalRead(BUTTON);
-  // digitalWrite(led, HIGH);
-
-  // Teste botão
-  // if (estadobotao == LOW) {
-  //   contador = contador++;
-  //  Serial.println("Botão Pressionado");
-  //   client.send(contador);
-  // }
-  //delay(300);
-
-  readSensorFluke();
-
 }
 
 
-void readSensorFluke() {
-  //executa a contagem de pulsos uma vez por segundo
-  if ((millis() - tempo_antes) > 1000) {
+/**
+   Método que faz a leitura e interpretação dos dados recebidos pelo sensor
+*/
+void readSensorFlow() {
+  currentMillis = millis();
+  //Leitura sensor
+  if (currentMillis - previousMillis > interval) {
 
-    //desabilita a interrupcao para realizar a conversao do valor de pulsos
-    detachInterrupt(INTERRUPCAO_SENSOR);
+    pulse1Sec = pulseCount;
+    pulseCount = 0;
 
-    //conversao do valor de pulsos para L/min
-    fluxo = ((1000.0 / (millis() - tempo_antes)) * contador) / FATOR_CALIBRACAO;
+    // Calcula o número de milissegundos que se passaram desde a última execução para dimensionar a saída.
+    // A variável CalibrationFactor serve para dimensionar a saída com base no número de pulsos por segundo por
+    // unidades de medida (litros / minuto) vindo do sensor
+    flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
+    previousMillis = millis();
 
-    //exibicao do valor de fluxo
-    Serial.print("Fluxo de: ");
-    Serial.print(fluxo);
-    Serial.println(" L/min");
+    // Divide a vazão em litros / minuto por 60 para determinar quantos litros têm
+    // passou pelo sensor neste intervalo de 1 segundo, então multiplique por 1000 para
+    // converter para mililitros.
+    flowMilliLitres = (flowRate / 60) * 1000;
+    flowLitres = (flowRate / 60);
 
-    //calculo do volume em L passado pelo sensor
-    volume = fluxo / 60;
+    // Add the millilitres passed in this second to the cumulative total
+    totalMilliLitres += flowMilliLitres;
+    totalLitres += flowLitres;
 
-    //armazenamento do volume
-    volume_total += volume;
-
-    //exibicao do valor de volume
-    Serial.print("Volume: ");
-    Serial.print(volume_total);
-    Serial.println(" L");
-    Serial.println();
-
-    //reinicializacao do contador de pulsos
-    contador = 0;
-
-    //atualizacao da variavel tempo_antes
-    tempo_antes = millis();
-
-    //contagem de pulsos do sensor
-    attachInterrupt(INTERRUPCAO_SENSOR, contador_pulso, FALLING);
-
+    // Print the flow rate for this second in litres / minute
+    if (DEBUG) {
+      Serial.print("Litros: ");
+      Serial.print(float(totalLitres));  // Print the integer part of the variable
+      Serial.print("L");
+      Serial.println("\t");       // Print tab space
+    }
   }
-}
 
-//funcao chamada pela interrupcao para contagem de pulsos
-void contador_pulso() {
-
-  contador++;
-
+  if (currentMillis - previousMillisService > oneHour) {
+    client.send(String(totalLitres));
+    Serial.println("Enviado ao servidor");
+    previousMillisService = millis();
+  }
 }
