5b1194fef0cf1bbf71ca9dc4ccef164d625a9bed
Isabella Galvão <isabella_asg@hotmail.com>
2021-11-03 21:54:38 -0300

    feat - insert dados mysql

--- node-websocket-server/websocket.js
+++ node-websocket-server/websocket.js
@@ -1,5 +1,15 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+var date = new Date();
+var createdAt = date.getFullYear()+'-'+date.getMonth()+'-'+date.getDate()+' '+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds()
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'isabella',
+    password: '123456',
+    database: 'bdarduino'
+});
 
 //Porta que o server irá escutar
 const port = 80;
@@ -15,7 +25,7 @@ server.listen(port, () => {
 
 //Cria o WebSocket server
 wsServer = new WebSocketServer({
-  httpServer: server
+    httpServer: server
 });
 
 //Chamado quando um client deseja conectar
@@ -26,16 +36,31 @@ wsServer.on('request', (request) => {
     //Aceita a conexão do client
     let client = request.accept(null, request.origin);
 
-    //Chamado quando o client envia uma mensagem
+    //Chamado quando o client envia um valor
     client.on('message', (message) => {
         //Se é uma mensagem string utf8
-        if (message.type === 'utf8') {
+        if (message.type === 'utf8' ) {
             //Mostra no console a mensagem
             console.log(message.utf8Data);
+
+            if (message.utf8Data !== 'Hello Server' ) {
+                const volume = message.utf8Data;
+
+                //INSERT NA TABELA
+                const sqlInsert = 'INSERT INTO tables(sensor, createdAt, updatedAt) VALUES (?,?,?);';
+                const values = [volume , createdAt, '2021-09-20 00:15:28'];
+                connection.query(sqlInsert, values, (err, rows) => {
+                    if (!err) {
+                        console.log('Sucesso na query', rows);
+                    } else {
+                        console.log('Erro na query');
+                    }
+                });
+            }
         }
     });
 
-    // //Cria uma função que será executada a cada 1 segundo (1000 millis) para enviar o estado do led
+    // Teste com led - Cria uma função que será executada a cada 1 segundo (1000 millis) para enviar o estado do led
     // let interval = setInterval(() => {
     //     //Envia para o client "ON" ou "OFF" dependendo do estado atual da variável state
     //     client.sendUTF(state? "ON" : "OFF");
@@ -46,7 +71,7 @@ wsServer.on('request', (request) => {
     //Chamado quando a conexão com o client é fechada
     client.on('close', () => {
         console.log("Conexão fechada");
-        //Remove o intervalo de envio de estado
-        clearInterval(interval);
+        //Remove o intervalo de envio de estado (teste com led)
+        //clearInterval(interval);
     });
 });
